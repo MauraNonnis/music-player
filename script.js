a34358fc9504175932b22b9bdc5f05c4905edd37
Maurizia Nonnis <71176277+MauraNonnis@users.noreply.github.com>
2021-05-24 21:50:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,39 +3,51 @@ let play = document.querySelector('#play');
 let next = document.querySelector('#next');
 let title = document.querySelector('#title');
 let slider = document.querySelector('#duration_slider');
+let backwardBtn = document.getElementById("backwardBtn")
+let playBtn = document.getElementById("playBtn")
+let fwdBtn = document.getElementById("fwdBtn")
+
 let track_image = document.querySelector('#track_image');
 let artist = document.querySelector('#artist');
 let image = document.getElementById("track_image")
+
+let bulb = document.getElementById("bulb")
+
+let mainDiv = document.getElementById("main-div")
+
 let timer;
 let autoplay = 0;
+
 let index_no = 0;
-let playingSong = false;
+let Playing_song = false;
+
+//create a audio Element
 let track = document.createElement('audio');
 
 
-let allSongs = [
+let All_song = [
    {
      name: "Concert no. 21",
      path: "music/Andante.mp3",
-     img: "pics/mozart.jpg",
+     img: "img/mozart.jpg",
      singer: "W.A. Mozart"
    },
    {
      name: "Spring",
      path: "music/Spring.mp3",
-     img: "pics/vivaldi.jpg",
+     img: "img/vivaldi.jpg",
      singer: "Antonio Vivaldi"
    },
    {
      name: "Concert no. 3",
      path: "music/Concerto-no-3.mp3",
-     img: "pics/bach.jpg",
+     img: "img/bach.jpg",
      singer: "Sebastian Bach"
    },
    {
      name: "Symphony no. 40",
      path: "music/Symphony no. 40.mp3",
-     img: "pics/mozart.jpg",
+     img: "img/mozart.jpg",
      singer: "W.A. Mozart"
    }
 ];
@@ -46,11 +58,13 @@ let allSongs = [
 function load_track(index_no){
 	clearInterval(timer);
 	reset_slider();
-	track.src = allSongs[index_no].path;
-	title.innerHTML = allSongs[index_no].name;	
-	track_image.src = allSongs[index_no].img;
-    	artist.innerHTML = allSongs[index_no].singer;
-    	track.load();
+
+	track.src = All_song[index_no].path;
+	title.innerHTML = All_song[index_no].name;	
+	track_image.src = All_song[index_no].img;
+    artist.innerHTML = All_song[index_no].singer;
+    track.load();
+
 	timer = setInterval(range_slider ,1000);
 	
 }
@@ -58,7 +72,7 @@ function load_track(index_no){
 load_track(index_no);
 
 function justplay(){
- 	if(playingSong==false){
+ 	if(Playing_song==false){
  		playsong();
 
  	}else{
@@ -73,19 +87,19 @@ function reset_slider(){
 
 function playsong(){
   track.play();
-  playingSong = true;
+  Playing_song = true;
   play.innerHTML = '<i class="fas fa-pause"></i>';
 }
 
 
 function pausesong(){
 	track.pause();
-	playingSong = false;
+	Playing_song = false;
 	play.innerHTML = '<i class="fas fa-play"></i>';
 }
 
 function next_song(){
-	if(index_no < allSongs.length - 1){
+	if(index_no < All_song.length - 1){
 		index_no += 1;
 		load_track(index_no);
 		playsong();
@@ -104,7 +118,7 @@ function previous_song(){
 		playsong();
 
 	}else{
-		index_no = allSongs.length;
+		index_no = All_song.length;
 		load_track(index_no);
 		playsong();
 	}
@@ -118,16 +132,34 @@ function change_duration(){
 
 function range_slider(){
 	let position = 0;
-	if(!isNaN(track.duration)){
-		position = track.currentTime * (100 / track.duration);
-		slider.value =  position;
-	}
+        
+        // update slider position
+		if(!isNaN(track.duration)){
+		   position = track.currentTime * (100 / track.duration);
+		   slider.value =  position;
+	      }
+
+       
+       // function will run when the song is over
        if(track.ended){
        	 play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
            if(autoplay==1){
 		       index_no += 1;
 		       load_track(index_no);
 		       playsong();
-       		}
-       }
+           }
+	    }
+}
+
+
+bulb.addEventListener("click", toggleTheme)
+
+function toggleTheme() {
+	document.body.classList.toggle("light-theme")
+	mainDiv.classList.toggle("light-theme")
+	backwardBtn.classList.toggle("light-theme")
+	playBtn.classList.toggle("light-theme")
+	fwdBtn.classList.toggle("light-theme")
+	bulb.classList.toggle("light-theme")
+	//slider.classList.toggle("light-theme")
 }
